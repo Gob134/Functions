5cdb4523c4d7971670f9e9bc91ca168c0a9a2cf9
Валентин <@COMP>
2023-06-15 19:49:22 +1000

    .

--- Arrays/Source.cpp
+++ Arrays/Source.cpp
@@ -2,27 +2,53 @@
 using namespace std;
 #define tab "\t";
 
-double fact(int a)
+void main()
 {
-    if (a < 0)
-        return 0;
-    if (a == 0)
-        return 1;
-    else
-        return a * fact(a - 1);
-}
+	setlocale(LC_ALL, "");
+	const int SIZE = 3;
+	double arr[SIZE] = { 5,8,9 };
+	double matrix_one[SIZE][SIZE] = {};
+	double matrix_two[SIZE][SIZE] = {};
+	double matrix_res[SIZE][SIZE] = {};
 
-int main()
-{
-    int a, b;
-    setlocale(LC_ALL, "");
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+		{
+			matrix_one[i][j] = rand() % 10;
+			cout << matrix_one[i][j] << tab;
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+		{
+			matrix_two[i][j] = rand() % 10;
+			cout << matrix_two[i][j] << tab;
+		}
+		cout << endl;
+	}
 
-    cout << "¬ведите число: ";
-    cin >> a;
-    cout << "‘акториал числа " << a << " = " << fact(a) << tab;
-    return 0;
-
-    cout << "¬ведите число: "; cin >> a;
-    cout << "¬ведите степень: "; cin >> b;
-    cout << a << " в степени " << b << " = " << pow(a, b);
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+		{
+			double res = 0;
+			for (int k = 0; k < SIZE; k++)
+			{
+				res += matrix_one[i][k] * matrix_two[i][k];
+			}
+			matrix_res[i][j] += res;
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+		{
+			cout << matrix_res[i][j] << tab;
+		}
+		cout << endl;
+	}
 }
